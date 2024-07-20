f4428125a03d7a930c761d1ac63680123b6bb2a2
Somi <somi8945@naver.com>
2024-07-20 21:21:29 +0900

    @Configuration - CGLIB 기술 singleton 보장

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -23,4 +23,9 @@ public class OrderServiceImpl implements OrderService{
 
         return new Order(memberId, itemName, itemPrice, disCountPrice);
     }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
